4b27fbddeeed06990b88b8d6b9bca43b1c9d5bb9
Thiago S. Marcelino <thiagodsmarcelino@gmail.com>
2026-01-06 15:24:36 -0300

    feat: refactor Request struct and implement request line parsing in a separated function

--- internal/request/request.go
+++ internal/request/request.go
@@ -13,10 +13,12 @@ type RequestLine struct {
 }
 
 type Request struct {
-	RequestLine RequestLine
+	RequestLine *RequestLine
 }
 
+// Theoretically, \n could be the separator as well if the first line ends with it, but for now we only support \r\n.
 var SEPARATOR = []byte("\r\n")
+
 var HTTP_VERSION_PREFIX = []byte("HTTP/")
 var VALID_METHODS = [][]byte{
 	[]byte("GET"),
@@ -57,36 +59,48 @@ func isValidVersion(version []byte) bool {
 	return false
 }
 
-func RequestFromReader(r io.Reader) (*Request, error) {
-	text, err := io.ReadAll(r)
-	if err != nil {
-		return nil, err
-	}
+func parseRequestLine(buffer []byte) (requestLine *RequestLine, rest []byte, err error) {
+	// SP = single space
+	// Request line format: <METHOD> <SP> <REQUEST_TARGET> <SP> HTTP/<VERSION>
 
-	before, _, found := bytes.Cut(text, SEPARATOR)
+	before, after, found := bytes.Cut(buffer, SEPARATOR)
 	if !found {
-		return nil, MALFORMED_REQUEST_ERROR
+		return nil, after, MALFORMED_REQUEST_ERROR
 	}
 
 	parts := bytes.Split(before, []byte(" "))
 	if len(parts) != 3 {
-		return nil, MALFORMED_START_LINE_ERROR
+		return nil, after, MALFORMED_START_LINE_ERROR
 	}
 
 	method := parts[0]
 	if !isValidMethod(method) {
-		return nil, INVALID_METHOD_ERROR
+		return nil, after, INVALID_METHOD_ERROR
 	}
 
 	version := bytes.TrimPrefix(parts[2], HTTP_VERSION_PREFIX)
 	if !isValidVersion(version) {
-		return nil, INVALID_VERSION_ERROR
+		return nil, after, INVALID_VERSION_ERROR
 	}
 
 	rl := RequestLine{
 		Method:        string(parts[0]),
 		RequestTarget: string(parts[1]),
 		HttpVersion:   string(version),
+	}
+
+	return &rl, buffer[len(before)+len(SEPARATOR):], nil
+}
+
+func RequestFromReader(r io.Reader) (*Request, error) {
+	buffer, err := io.ReadAll(r)
+	if err != nil {
+		return nil, err
+	}
+
+	rl, _, err := parseRequestLine(buffer)
+	if err != nil {
+		return nil, err
 	}
 
 	res := &Request{
